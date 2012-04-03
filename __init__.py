766d2b7eeda56d80130a6aac9b37b365df76f2c0
ksreethulasi <ksreethulasi@zeomega.com>
2012-04-03 11:53:27 +0530

    school sample

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+from OFS.Folder import Folder
+
+def add_school(context):
+ """add school"""
+    context._setObject('school',school())
+class school (Folder):
+  """set school"""
+     meta_type='school'
+
 def initialize(context):
-    print 'hello'
+    context.registerClass(school,constructors=(add_school,))
 
